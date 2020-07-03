2a9206f1bbce4de7adf5396df6c6530a07cc4143
Diego van Haaster <diegomvh@gmail.com>
2020-07-03 17:15:31 -0300

    Update api

--- src/app/trippin/trippin.config.ts
+++ src/app/trippin/trippin.config.ts
@@ -1,4 +1,4 @@
-﻿import { DATE_PARSER, Configuration, DECIMAL_PARSER } from 'angular-odata';
+﻿import { Configuration, DATE_PARSER, DECIMAL_PARSER } from 'angular-odata';
 
 //#region ODataApi Imports
 import { TripPinSchema } from './Microsoft/OData/SampleService/Models/TripPin/trippin.schema';
@@ -7,7 +7,7 @@ import { TripPinSchema } from './Microsoft/OData/SampleService/Models/TripPin/tr
 export const TripPinConfig = {
   name: 'TripPin',
   serviceRootUrl: 'https://services.odata.org/V4/TripPinServiceRW/',
-  creation: new Date('2020-06-22T22:09:20.7675441-03:00'),
+  creation: new Date('2020-07-03T17:14:18.5013542-03:00'),
   schemas: [
     TripPinSchema
   ],
