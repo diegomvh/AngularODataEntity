8ed8b5fcb795c606608d0ba1f8f43231842e4b22
Diego van Haaster <diegomvh@gmail.com>
2020-12-08 06:49:57 -0300

    Update northwind

--- src/app/northwind/northwind.config.ts
+++ src/app/northwind/northwind.config.ts
@@ -9,7 +9,7 @@ export const NorthwindConfig = {
   serviceRootUrl: 'https://services.odata.org/V4/Northwind/Northwind.svc/',
   name: 'Northwind',
   version: '4.0',
-  creation: new Date('2020-12-04T07:16:37.6595135-03:00'),
+  creation: new Date('2020-12-08T06:49:44.8893607-03:00'),
   schemas: [
     NorthwindModelSchema,
     ModelSchema
